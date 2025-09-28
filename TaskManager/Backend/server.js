ae01cd5c007106f6d4c7028c2048978c5d795cc6
Aman Jaiswal <143282413+amanjaiswal26@users.noreply.github.com>
2025-09-28 13:44:10 +0530

    Update server.js

--- TaskManager/Backend/server.js
+++ TaskManager/Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  origin: process.env.CORS_ORIGIN,
   credentials: true
 }));
 
@@ -38,3 +38,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
